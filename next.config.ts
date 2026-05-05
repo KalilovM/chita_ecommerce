ffadecb06ea20cd2aa0600c3a783b40bb5baad29
KalilovM <mn.kalilov@gmail.com>
2026-05-06 00:39:39 +0200

    Allow Server Actions from gala75.ru proxy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["gala75.ru", "www.gala75.ru"],
+      bodySizeLimit: "20mb",
+    },
+  },
 };
 
 export default nextConfig;
